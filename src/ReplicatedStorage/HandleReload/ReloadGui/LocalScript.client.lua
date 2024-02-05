772418a51d40a082ad923ef2b6987bc35f62f3d4
Micah <micah@uplift.games>
2024-02-05 11:57:45 -0800

    Update with new syncback results

--- src/ReplicatedStorage/HandleReload/ReloadGui/LocalScript.client.lua
+++
@@ -1,9 +0,0 @@
-local ScreenGui = script.Parent
-local Progress = ScreenGui:FindFirstChild("Progress", true)
-local Container = ScreenGui:FindFirstChild("Container", true)
-local function ResizeProgress()
-	Progress.Size = UDim2.new(0, Container.AbsoluteSize.X, 0, Container.AbsoluteSize.Y)
-end
-
-Container:GetPropertyChangedSignal("AbsoluteSize"):connect(ResizeProgress)
-ResizeProgress()
